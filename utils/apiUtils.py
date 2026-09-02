49d412c76ccd9eb7a9efb8e230ba8f9f85123e63
Isabel Galdamez <isabelgaldamez@sbcglobal.net>
2026-09-02 11:20:32 -0700

    api testing

--- utils/apiUtils.py
+++ utils/apiUtils.py
@@ -5,3 +5,15 @@ def getApiData(url, opHeader=None):
     headers = (headers|opHeader) if isinstance(opHeader, dict) else headers
     response = requests.get(url, verify=False, headers=opHeader)
     return response
+
+def postApiData(url, body):
+    headers = {"Content-Type" : "application/json"}
+    print('\nReqURL: ' + url)
+    print('\nReqBody: ' + json.dumps(body))
+    return requests.post(url, verify=False, headers = headers, json = body)
+
+def patchApiData(url, payload):
+    headers = {"Content-Type": "application/json"}
+    return requests.put(url, verify=False, headers=headers, json=payload)
+
+
